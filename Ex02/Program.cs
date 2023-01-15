5596b118ecd09bdc92683fb792e819f250b1de48
Kate <katekrupnik@gmail.com>
2023-01-16 01:19:57 +0300

    for Ex02

--- Ex02/Program.cs
+++ Ex02/Program.cs
@@ -3,14 +3,31 @@
 //значения b1, k1, b2 и k2 задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-
+//1.create points
 (int x, int y) CreatePoint()
 {
-  Console.
+  Console.WriteLine("Введите значение первой точки прямой: ");
   int x = int.Parse(Console.ReadLine());
+  Console.WriteLine("Введите значение второй прямой точки: ");
   int y = int.Parse(Console.ReadLine());
-  return (1,2);
+  return (x,y);
+}
+//3.
+(int pointX, int pointY)GetCrossOfLines( int linePointB1, int linePointK1, int linePointB2, int linePointK2)
+{
+
 }
 
-var B1point = CreatePoint();
-var K1point = CreatePoint();
+
+
+
+
+
+
+
+
+
+var pointsOfline1 = CreatePoint();
+Console.WriteLine($"Координаты первой прямой:{pointsOfline1}");
+var pointsOfline2 = CreatePoint();
+Console.WriteLine($"Координаты второй прямой:{pointsOfline2}");
